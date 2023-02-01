d71ac4e32253d6cc87ceceb2a4143cc3ba11de83
sindrebrurberg <sindre@brurberg.no>
2023-02-02 08:37:47 +0900

    Adding params to websockets

--- websocket/base.go
+++ websocket/base.go
@@ -7,14 +7,15 @@ import (
 	"nhooyr.io/websocket"
 )
 
-func Websocket(r *gin.RouterGroup, path string, wsfunc func(ctx context.Context, conn *websocket.Conn) bool) {
+func Websocket(r *gin.RouterGroup, path string, wsfunc func(ctx context.Context, conn *websocket.Conn, params gin.Params) bool) {
 	r.GET(path, func(c *gin.Context) {
+		p := c.Params
 		s, err := websocket.Accept(c.Writer, c.Request, nil)
 		if err != nil {
 			log.AddError(err).Fatal("while accepting websocket")
 		}
 		defer s.Close(websocket.StatusNormalClosure, "") //Could be smart to do something here to fix / tell people of errors.
-		for wsfunc(c.Request.Context(), s) {
+		for wsfunc(c.Request.Context(), s, p) {
 		}
 	})
 }
